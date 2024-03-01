3fd6b307b3d5cfb1e5105a69fb50a1cece4893d9
ap <pokrov1991@gmail.com>
2024-03-01 12:16:43 +0300

    Handlebars test

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 import './style.css'
+import './main.scss'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+
 
 import { obj1 } from "./test1.js";
 import { obj2 } from "./test2.js";
 console.log('obj1', obj1)
 console.log('obj2', obj2)
 
+
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
+  <div class="app">
+    <a class="app__logo" href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,36 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
 
+
+// const pageData = {
+//   '/index.html': {
+//     title: 'Main Page',
+//   },
+//   '/nested/subpage.html': {
+//     title: 'Sub Page',
+//   },
+// };
+
+// export default defineConfig({
+//   plugins: [
+//     handlebars({
+//       context(pagePath) {
+//         return pageData[pagePath];
+//       },
+//     })
+//   ],
+// })
+
+
+
 export default defineConfig({
-  plugins: [handlebars()],
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, 'partials'),
+    }),
+  ],
 }) 
